764582305d1746c91f4789c4d81272a56202966e
Jacob Turner <poppa.bear.joe@gmail.com>
2015-06-04 18:58:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var port = process.env.EXPRESS_PORT || 9001;
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.all('*', function(req, res, next) {
     res.sendFile('/public/index.html', { root: __dirname });
 });
